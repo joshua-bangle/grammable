b8237b34dfb925c890467892382a02b8da0dcaad
Joshua Bangle <joshua.bangle@gmail.com>
2019-09-30 00:50:25 +0000

    in the middle of adding comments

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -151,8 +151,13 @@ RSpec.describe GramsController, type: :controller do
     it "should successfully create a new gram in our database" do
       user = FactoryBot.create(:user)
       sign_in user
+      post :create, params: {
+        gram: {
+          message: 'Hello!',
+          picture: fixture_file_upload("/picture.png", 'image/png')
+        }
+      }
 
-      post :create, params: { gram: { message: 'Hello!' } }
       expect(response).to redirect_to root_path
 
       gram = Gram.last
